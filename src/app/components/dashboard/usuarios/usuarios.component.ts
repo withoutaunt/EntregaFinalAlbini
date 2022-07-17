b49ffe5791d6f96d9ce7c522a6b1772f399da7a3
withoutaunt <cinnalb@gmail.com>
2022-07-17 19:26:39 -0300

    cambiosagregar

--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuariosService } from '../../../services/usuarios.service';
 import { Usuarios } from '../../../interfaces/usuarios';
+import { CrearUsuarioComponent } from '../crear-usuario/crear-usuario.component';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 
 @Component({
@@ -10,7 +12,16 @@ import { Usuarios } from '../../../interfaces/usuarios';
 })
 export class UsuariosComponent implements OnInit {
   usuariosList: Usuarios[] = [];
-  constructor( private UsuariosService: UsuariosService) { }
+  constructor( private UsuariosService: UsuariosService, private dialog: MatDialog ) { }
+
+  openDialog() {
+   this.dialog.open(CrearUsuarioComponent);{
+
+   }
+
+   
+  }
+ 
 
   displayedColumns: string[] = ['id', 'nombreDeUsuario', 'email', 'telefono'];
   // dataSource = ELEMENT_DATA;
