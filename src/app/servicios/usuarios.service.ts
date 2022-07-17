3609754447185e0c1e75dd3e7384b6f1cbab927d
withoutaunt <cinnalb@gmail.com>
2022-07-17 00:33:51 -0300

    listausuarios lista

--- src/app/servicios/usuarios.service.ts
+++ src/app/servicios/usuarios.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { Usuarios } from '../models/usuarios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
-  root_url= 'https://62c1291beff7f7856f0a0671.mockapi.io/api/v1/usuarios'
+  root_url= 'https://62c1291beff7f7856f0a0671.mockapi.io/api/v1/usuarios/'
   constructor(private http: HttpClient) { }
 
   getUsuariosList(): Observable<Usuarios[]>{
     return this.http.get <Usuarios[]> (this.root_url);
   }
+  getUsuarioSolo(id:number): Observable<Usuarios>{
+    return this.http.get <Usuarios>(this.root_url + id)
+  }
+  
+  
 }
