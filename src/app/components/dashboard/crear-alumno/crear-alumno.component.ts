d66568a5e6d83659b0149d7d0341853b8a2733af
withoutaunt <cinnalb@gmail.com>
2022-07-18 22:12:12 -0300

    formalumnos

--- src/app/components/dashboard/crear-alumno/crear-alumno.component.ts
+++ src/app/components/dashboard/crear-alumno/crear-alumno.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-crear-alumno',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-alumno.component.css']
 })
 export class CrearAlumnoComponent implements OnInit {
+  formCrearAlumno: FormGroup;
+  submitted = false;
+  UsuariosService: any;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder,
+    private_alumnosService: AlumnosService) {
+    this.formCrearAlumno = this.fb.group({
+      nombre: ['', Validators.required],
+      sexo: ['', Validators.required],
+      apellido: ['', Validators.required],
+      cursos: ['', Validators.required],
+     
+
+
+    })
+  }
 
   ngOnInit(): void {
   }
-
-}
+  //VALIDACION PARA QUE SE COMPLETEN TODOS LOS CAMPOS EN EL FORM 
+  aceptarAlumno(){
+    console.log(this.formCrearAlumno.value);
+  }
+  
+ }
